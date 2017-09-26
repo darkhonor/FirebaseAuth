18c56ed8b0944bb8daf9c3edb1c870837302ae4a
Alex Ackerman <aackerman@darkhonor.com>
2017-09-26 20:49:01 +0900

    Updated Travis config--changed build target to 25 Added progress dialog and updated interface to include Sign-Out button

--- app/src/main/java/com/darkhonor/android/firebaseauth/MainActivity.java
+++ app/src/main/java/com/darkhonor/android/firebaseauth/MainActivity.java
@@ -1,11 +1,15 @@
 package com.darkhonor.android.firebaseauth;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,9 +34,14 @@ public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     SignInButton mSignInButton;
+
     TextView mStatusTextView;
     GoogleApiClient mGoogleApiClient;
     FirebaseAuth mAuth;
+    Button mSignOutButton;
+
+    @VisibleForTesting
+    public ProgressBar mProgressBar;
 
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 9001;
@@ -55,6 +64,10 @@ public class MainActivity extends AppCompatActivity implements
         mStatusTextView = (TextView) findViewById(R.id.status_textview);
         mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
         mSignInButton.setOnClickListener(this);
+        mSignOutButton = (Button) findViewById(R.id.sign_out_button);
+        mSignOutButton.setOnClickListener(this);
+        mProgressBar = (ProgressBar) findViewById(R.id.indeterminateBar);
+        mProgressBar.setVisibility(View.GONE);
 
         // Initialize FirebaseAuth instance
         mAuth = FirebaseAuth.getInstance();
@@ -65,6 +78,9 @@ public class MainActivity extends AppCompatActivity implements
         switch (view.getId()) {
             case R.id.sign_in_button:
                 signIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
                 break;
         }
     }
@@ -90,7 +106,7 @@ public class MainActivity extends AppCompatActivity implements
                 new ResultCallback<Status>() {
                     @Override
                     public void onResult(@NonNull Status status) {
-                       // updateUI(null);
+                        updateUI(null);
                     }
                 });
     }
@@ -108,7 +124,7 @@ public class MainActivity extends AppCompatActivity implements
         if (result.isSuccess()) {
             GoogleSignInAccount acct = result.getSignInAccount();
             firebaseAuthWithGoogle(acct);
-            mStatusTextView.setText("Hello, " + acct.getDisplayName());
+
         } else {
 
         }
@@ -116,11 +132,13 @@ public class MainActivity extends AppCompatActivity implements
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
         Log.d(TAG, "firebaseAuthWithGoogle: " + account.getId());
+        mProgressBar.setVisibility(View.VISIBLE);
         AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        mProgressBar.setVisibility(View.GONE);
                         if (task.isSuccessful()) {
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
@@ -141,10 +159,23 @@ public class MainActivity extends AppCompatActivity implements
 
     private void updateUI(FirebaseUser user) {
         if (user != null) {
-            mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
+            Log.d(TAG, "User signed in: {user.getEmail()}");
+            mStatusTextView.setText("Hello, " + user.getDisplayName() + " (" +
+                    user.getEmail() +")");
             findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
         } else {
+            Log.d(TAG, "No User signed in.");
+            mStatusTextView.setText("Please Sign In");
             findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
         }
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mProgressBar.setVisibility(View.GONE);
     }
 }
